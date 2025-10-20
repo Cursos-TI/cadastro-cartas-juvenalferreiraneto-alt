aebe52bb2a448bee4bc5669d61920fea7166675c
juvenalferreiraneto-alt <juvenalferreiraneto@gmail.com>
2025-10-20 20:31:16 -0300

    Atulização supertriunfo.c

--- supertriunfo.c
+++ supertriunfo.c
@@ -4,7 +4,7 @@ int main() {
 
     char estado1, estado2;//Variáveis para estado contendo uma letra(A,B,C...)
     char cidade1[20], cidade2[20];//Variáveis tipo string com no máximo 20 caracteres
-    char cod_carta1[4], cod_carta2[4];//Variáveis string composta pelo estado e 3 números(ex. A001)
+    char cod_carta1[3], cod_carta2[3];//Variáveis string composta pelo estado e 2 números(ex. A01)
     int populacao1, populacao2;//Variáveis numero inteiro refererente a quantidade de habitantes    
     int num_ponto_turistico1, num_ponto_turistico2;//Variáveis numero inteiro referente quantidade de pontos turísticos
     float area1, area2;//Variáveis para registrar a área da cidade em km²
@@ -20,13 +20,13 @@ int main() {
     printf("Digite o nome da Cidade: ");//solicita ao usuário o Nome da Cidade
     scanf("%s", cidade1);//Guarda na variável o nome digitado
 
-    printf("Digite o código da Carta 1: ");//solicita que se digite o código da carta composto pela letra do Estado e sequencia de 3 números
+    printf("Digite o código da Carta 1: ");//solicita que se digite o código da carta composto pela letra do Estado e sequencia de 2 números
     scanf("%s", cod_carta1);//Guarda o código digitado na variável
 
-    printf("Digite a população da cidade: ");//solicita e se digite a quantidade de habitantes da cidade
+    printf("Digite a população da cidade: ");//solicita que se digite a quantidade de habitantes da cidade
     scanf("%i", &populacao1);//Guarda o valoor da populção
 
-    printf("Digite a área da cidade: ");//solicta que se a área quilometros quandrados da cidade
+    printf("Digite a área da cidade: ");//solicta que se digite a área em quilometros quandrados da cidade
     scanf("%f", &area1);//guarda na variável o valor digitado
 
     printf("Digite o PIB da cidade: ");//solcita que se digite o PIB da cidade
@@ -67,8 +67,8 @@ int main() {
     printf("Cidade: %s\n", cidade1);
     printf("População: %d habitantes\n", populacao1);//exibe o valor da vaiábel concatenado com um complemento
     printf("Área: %.2f km²\n", area1);//exibe o valor da área da cidade com complemento
-    printf("PIB: %.2f bilhões de reais\n", PIB1);
-    printf("Pontos Turísticos: %i\n\n", num_ponto_turistico1);
+    printf("PIB: %.2f bilhões de reais\n", PIB1);\\exibe valor digitado concatenando com complemento
+    printf("Pontos Turísticos: %i\n\n", num_ponto_turistico1);\\exibe a quantidade de ponstos turíscos da cidade
     //fim da informções da carta 1
     //Início das informações da carta 2 
     printf("CARTA 2\n");
@@ -80,5 +80,6 @@ int main() {
     printf("PIB: %.2f Bilhões de reais\n", PIB2);
     printf("Pontos Turísticos: %i\n", num_ponto_turistico2);
     //finaliza a exibição das informações da carta 2
-    return 0;//encerr o programa
+return 0;//encerra o programa
 }
+
